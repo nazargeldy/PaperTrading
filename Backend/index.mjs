abb253f2a8267b2e2a96a5f3851964dcbd78f627
daryan052205 <zocdarel@gmail.com>
2024-10-22 18:17:03 -0700

    new file:   .gitignore 	deleted:    Backend/.gitignore 	modified:   Backend/index.mjs 	modified:   Backend/package-lock.json 	modified:   Backend/package.json 	new file:   Frontend/views/home.ejs 	new file:   Frontend/views/partials/head.ejs

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -2,33 +2,50 @@ import express from 'express';
 import fetch  from 'node-fetch';
 import dotenv from 'dotenv';
 import moment from 'moment-timezone';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+// Get the current directory of the index.mjs file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set the 'views' folder to the 'frontend/views' directory
+app.set('views', path.join(__dirname, '../Frontend/views'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Serve static files (e.g., images, CSS, JS) from the 'public' folder
+app.use(express.static(path.join(__dirname, '../public')));
 dotenv.config();
 
 
-const app = express();
+
 const apiKey = process.env.KEY;// varible for the api key
-
-
 
 
 
 app.get('/', async (req, res) => {
   try {
 
-    const newYorkDate = moment().tz("America/New_York").format('YYYY-MM-DD');
-  
+  //  const newYorkDate = moment().tz("America/New_York").format('YYYY-MM-DD');
+  const newYorkDate='2024-10-16';
 
     // Make API call using node-fetch
     const response = await fetch(`https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${newYorkDate}?adjusted=true&include_otc=false&apiKey=${apiKey}`);
     const data = await response.json();
-    console.log(apiKey)
+
     // Send the data back to the client
-    res.json(data);
+    res.status(200);//everything perfect
+    res.render('home',{'data':data})
+
   } catch (error) {
     console.error(error);
     res.status(500).send('Error occurred while making the API call');
-  }
-});
+  }}
+);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
